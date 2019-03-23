4d684a984ba6eabf09d262b65e786fedf9a0100c
= <=>
2019-03-23 00:02:21 -0500

    Auth service is working and login the user and send token to frontend

--- src/auth/Auth.ts
+++ src/auth/Auth.ts
@@ -2,40 +2,60 @@ import { Request, Response } from "express";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 
-import User from "../models/User";
+import { keys } from "../config/keys";
 
-const JWT_SECRET: string = "Secret_Password";
+import User from "../models/User";
 
 class Auth {
   public users: any[] = [];
 
   constructor() {}
 
+  private static async findUser(email: string) {
+    try {
+      const userExists = await User.findOne({ email }).exec();
+      return userExists;
+    } catch (e) {
+      return e;
+    }
+  }
+
   public static async login(req: Request, res: Response) {
     const { email, pass } = req.body;
 
-    const userExists = await User.findOne({ email });
+    const userFinded = await Auth.findUser(email);
 
-    if (userExists !== null) {
-      if (pass) {
-        if (!bcrypt.compare(req.body.pass, userExists.password)) {
-          res.status(400).send({ message: "Invalid username or password" });
+    if (userFinded) {
+      try {
+        const compare = await bcrypt.compare(pass, userFinded.password);
+
+        if (!compare) {
+          res.status(400).json({
+            success: false,
+            message: "username or password incorrect"
+          });
         } else {
-          const tokenData = {
-            user: userExists,
-            date: new Date()
+          const payload = {
+            fname: userFinded.firstName,
+            lname: userFinded.lastName,
+            email: userFinded.email,
+            role: userFinded.role
           };
 
-          const token = jwt.sign(tokenData, JWT_SECRET, {
-            expiresIn: 60 * 60 * 24
+          const token = jwt.sign(payload, keys.SECRET_KEY, {
+            expiresIn: "1h"
           });
+
           res.status(200).json({
-            token: token
+            success: true,
+            token
           });
         }
-      } else {
-        res.status(400).json({ message: "the password cannot be empty" });
+      } catch (e) {
+        res.status(500).json({ message: "internal error", e });
       }
+    } else {
+      res.status(400).json({ message: "the user dosn't exists" });
     }
   }
 }
